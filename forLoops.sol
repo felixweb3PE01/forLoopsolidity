d79ff0b7b631c98e38e7b19981cb778101f839cc
Felix <felixweb3PE@gmail.com>
2026-01-13 04:07:48 +0100

    Update forLoops.sol

--- forLoops.sol
+++ forLoops.sol
@@ -5,13 +5,14 @@ contract loops{
     // uint256 public value; 
     uint256[] public  values;
 
+    // add values
    function add(uint256 x) public   {
 
     values.push(x);
     
    }
 
-
+   // Sum of Total[]
    function calcTotal() public view returns(uint){
 
     uint total;
@@ -24,7 +25,24 @@ contract loops{
     return total;
    }
 
+   // All Values[]
    function retrieve() public view returns(uint256[] memory){
     return values;
    }
+   // COunt
+    function retrieveCount() public view returns(uint256){
+     return values.length;
+    }
+    // Maximum
+    function getMax() view public returns (uint256) {
+
+       uint256 maxValue = values[0];
+       for (uint256 i=1; i< values.length; i++)
+       if(values[i]>maxValue){
+        maxValue=values[i];
+       }
+       return maxValue;
+    }
+
 }
+
